dd69242f6d36d77262523fd0b996b894f7f97c4a
Piero <macchioni+gh@gmail.com>
2020-04-03 16:19:30 +0200

    home

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Da Pancrazio',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Il ristorante Da Pancrazio, a Roma dal 1922, offre la migliore cucina romana nella curia del teatro di Pompeo.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
